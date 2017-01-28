b2db8ca57783049d50407e99b1e70da1643e8336
Yu-Ming Chen <yminchen@umich.edu>
2017-01-28 17:24:25 -0500

    found simulation bug

--- Events/groundEvent.m
+++ Events/groundEvent.m
@@ -14,10 +14,10 @@ function [position, isterminal, direction] = groundEvent(t,x)
     position(1)     = lamda(2);
     isterminal(1)   = 1;
     direction(1)    = -1;
-    position(2)     = 1;%hip(2);
+    position(2)     = hip(2);
     isterminal(2)   = 1;
     direction(2)    = -1;
-    position(3)     = 1;%knee(2);
+    position(3)     = knee(2);
     isterminal(3)   = 1;
     direction(3)    = -1;
     
